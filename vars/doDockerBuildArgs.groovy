dfc20e318ac979915153ffce99c8cdcf73d80889
Peter L. Berghold <pberghold@tenna.com>
2019-05-13 13:53:48 -0400

    Added a variant to doDockerBuild called doDockerBuildArgs to handle Docker builds with arguments to pass into the Dockerfile as arguments.

---
+++ vars/doDockerBuildArgs.groovy
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////
+//
+// File: doDockerBuild.groovy
+//
+// Description/Purpose:
+//     Encapsulation of docker build command for use with docker
+//     build pipelines
+//
+// Author:   Peter L. Berghold <pberghold@tenna.com>
+//
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+def void call(String tag,String dir='.',String[] args) {
+  def String command = "docker build "
+  for(String arg:args){
+    command = command + "--build-arg ${arg} "
+  }
+  command = command + "  -t " + tag + " " + dir
+  sh(script: command)
+  
+}
